585cbe822623060c84fe23752a2a032269239113
Nikita_Kulakov <kulakofnikita@gmail.com>
2024-09-19 11:39:51 +0300

    Обмен параметрами устройства

--- transmitter.py
+++ transmitter.py
@@ -30,10 +30,29 @@ class Transmitter:
     def get_signal_info(self) -> dict:
         return {
             "signalType": np.random.choise([1,2,3,4,5,6,7,8,9,10,51,52]), # тип радиосигнала/принадлежность[усл.ед.]
-            "signalFrequency": np.random.randint(0, 9223372036854775807, dtype=np.int64), # частота сигнала [Гц]
-            "signalAmplitude": np.random.randint(), # амплитуда сигнала [от -150дБм до 20дБм]
-            "signalWidth": np.random.randint(), # ширина сигнала [Гц]
+            "signalFrequency": int(np.random.randint(0, 9223372036854775807, dtype=np.int64)), # частота сигнала [Гц]
+            "signalAmplitude": int(np.random.randint(100)), # амплитуда сигнала [от -150дБм до 20дБм]
+            "signalWidth": int(np.random.randint(100)), # ширина сигнала [Гц]
             "signalDetectionType": np.random.choise([1,2]), # тип обнаружения [усл.ед.]
+        }
+    
+    def get_device_params(self) -> dict:
+        return {
+            "centerFrequency": int(np.random.randint(0, 9223372036854775807, dtype=np.int64)), # центральная частота сигнала [Гц]
+			"receiveSensitivity": int(np.random.randint(100)), # коэффициент усиления[дБ] (0 – АРУ)
+			"detectionBandwidth": int(np.random.randint(0, 9223372036854775807, dtype=np.int64)), # ширина полосы обнаружения [Гц]
+		
+			"idents": [{ 
+					"signalType": "", # тип идентификации
+					"signalDetectionType": "" # тип обнаружения
+	 	   		}]
+        }
+        
+    def get_device_mode(self) -> dict:
+        return {
+            "deviceType": self.device_type, # тип устройства 
+	        "deviceStatus": int(np.random.choice([1,2,3])), # статус устройства 
+	        "supressMode": int(np.random.randint(100)), # режим работы устройства обнаружения и идентификации [усл.ед.]
         }
 
     def get_uav_detection_data_frame(self, uav: UAV) -> str:
@@ -41,6 +60,18 @@ class Transmitter:
         frame.update(self.get_device_position_info())
         frame.update({"uav": uav.get_info()})
         return str(json.dumps(frame))
+    
+    def get_device_params_command(self) -> str:
+        frame = self.get_header(packet_type=2)
+        frame.update({"supressMode": int(np.random.randint(100))})
+        frame.update(self.get_device_params())
+        return str(json.dumps(frame))
 
     def get_parameter_querry(self) -> str:
         return str(json.dumps(self.get_header(packet_type=3)))
+    
+    def get_device_params_data_frame(self) -> str:
+        frame = self.get_header(packet_type=4)
+        frame.update(self.get_device_mode())
+        frame.update(self.get_device_params())
+        return str(json.dumps(frame))
